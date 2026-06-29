1b42c34561e825605a3c4c1ccdae64fcf32cf50a
Jay Logan <nagolpj@gmail.com>
2026-06-29 15:28:26 -0400

    Scroll to top on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import ScrollToTop from "./components/ScrollToTop";
 import Resume from "./pages/Resume";
 import BudgetBytes from "./pages/BudgetBytes";
 import NotFound from "./pages/NotFound";
@@ -16,6 +17,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/resume" element={<Resume />} />
